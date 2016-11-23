61e37fcf1f93eb5b9a465bd97084ce50a9f1e397
Ralph Eckhard <ralpheckhard@outlook.com>
2016-11-23 16:03:21 +0100

    Removed old code files to keep it clean ;)

--- new-natmapping.ps1
+++
@@ -1,5 +0,0 @@
-﻿# add static mapping
-Add-NetNatStaticMapping -NatName HO_Lab_Network -ExternalIPAddress 0.0.0.0 -ExternalPort 4001 -InternalIPAddress 172.16.0.88 -InternalPort 3389 -Protocol TCP
-
-# Open firewall on host server
-New-NetFirewallRule -Name TCP4001 -Protocol TCP -LocalPort 4001 -Action Allow -Enabled True
